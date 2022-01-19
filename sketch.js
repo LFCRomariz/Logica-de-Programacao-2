9742a2c0aaea4aa46fd0123581cd2e74f3f06aa4
LFCRomariz <romarizleonardo@gmail.com>
2022-01-19 13:39:49 -0300

    Jogo freeway em js

---
+++ sketch.js
@@ -0,0 +1,18 @@
+//Código principal do jogo
+function setup() {
+  createCanvas(500, 400);
+  somDaTrilha.loop();
+}
+
+function draw() {
+  background(imagemEstrada);
+  mostraAtor();
+  mostraCarro();
+  movimentaCarro();
+  movimentaAtor();
+  voltaPosicaoInicialDoCarro();
+  verificaColisao();
+  incluiPontos();
+  marcaPonto();
+}
+
